ffbf0694ea21620e79fcbbd5deeaa5df4e6382e5
Thanida Pholsukcharoen (27 Jun) <152356541+Thanida-P@users.noreply.github.com>
2025-09-23 20:28:57 +0700

    login/register implementation

--- api/api/settings.py
+++ api/api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'app_api',
+    'app_api.users'
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'users.User'
