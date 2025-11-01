fa98658be4a7a6850383b724a2e111419b066a79
thaniP <thanidapaige.p@gmail.com>
2025-11-02 02:37:38 +0700

    port configuration error fix

--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="frontend",
-    backend_port=8080,
+    backend_port=8091,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
