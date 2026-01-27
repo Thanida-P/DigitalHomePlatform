335c1550b35eef8f031a943a59f5e9e664316cb5
Cyntra <140875968+Cyntra-22@users.noreply.github.com>
2026-01-27 14:00:01 +0700

    Revert "update: backend changes"
    This reverts commit 0c400871482fcb73df3f3d07b3e49d289b3fd89d.

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import sys
-from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR.parent, 'local.env'))
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -63,8 +63,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get("DIGITAL_HOME_PLATFORM_URL", "http://localhost:3000"),
-    os.environ.get("SCENE_CREATOR_URL", "http://localhost:5175"),
+    os.environ.get("BASE_URL", ""),
 ]
 
 CORS_ALLOW_CREDENTIALS = True
--- frontend/frontend/config.py
+++ frontend/frontend/config.py
@@ -3,6 +3,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR.parent, 'local.env'))
+load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 API_BASE_URL = os.getenv("API_URL", "http://localhost:8001")
